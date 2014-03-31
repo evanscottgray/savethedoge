8fe2aa9d58643cf76d4201e23d7b97fb80cb0e5f
Evan Gray <evan.gray@rackspace.com>
2014-03-30 22:35:37 -0500

    spacing

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require 'rufus-scheduler'
 require 'redis'
 require 'i_heart_quotes'
 
-require 'pp'
-
 require './lib/message'
 require './lib/meds'
 
@@ -14,7 +12,6 @@ APP_CONFIG = JSON.parse(File.read('./config/config.json'))
 @twilioclient = Twilio::REST::Client.new APP_CONFIG['twilio']['account_sid'], APP_CONFIG['twilio']['auth_token']
 @scheduler = Rufus::Scheduler.new
 $redis = Redis.new
-
 $redis.set('meds_morning', "false")
 $redis.set('meds_evening', "false")
 
@@ -34,15 +31,11 @@ end
 post '/response' do
 
   if params[:Body].to_s.downcase.include? "yes" && Time.now.hour <= 10
-
     $redis.set('meds_evening', "false")
     $redis.set('meds_morning', "true")
-
   elsif params[:Body].to_s.downcase.include? "yes" && Time.now.hour > 12
-
     $redis.set('meds_evening', "true")
     $redis.set('meds_morning', "false")
-
   end
 
 end
